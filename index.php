196418c09d72e4df8e2340a31c55c2c8fb3af277
Mohamed Infaz <infaz9999@gmail.com>
2013-09-08 23:21:46 +0530

    The latest commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $email="infaz9999@gmail.com";
+        $default="localhost";
+        $size=1000;
+        
+        $grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) )."?d=".urlencode( $default )."&s=".$size;
+        $grav_url="http://www.gravatar.com/".md5($email);
+        
+        $str = file_get_contents( 'http://www.gravatar.com/46ab91522a3d3ea8fbb59831aecfb479.php' );
+        $profile = unserialize( $str );
+        if ( is_array( $profile ) && isset( $profile['entry'] ) ){
+                echo $profile['entry'][0]['displayName'];
+                echo "<br/>";
+                echo $profile['entry'][0]['emails'][0]['value'];
+                echo "<br/>";
+                echo $profile['entry'][0]['ims'][0]['value'];
+                echo "<br/>";
+                
+                
+                
+        }
+        
+
+        ?>
+       
+    </body>
+</html>
